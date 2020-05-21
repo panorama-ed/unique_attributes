c2feba1a384fcb037d2872e178f1a293128dce73
Jacob Evelyn <jevelyn@panoramaed.com>
2020-05-21 11:13:20 -0400

    INFRA-499 Modernize repo in many ways
    This commit:
    
    - adds Dependabot configuration and automation
    - removes Travis CI in favor of GitHub Actions tests
    - adds a Code of Conduct
    - updates the SQLite3 dependency version
    - updates the RuboCop configuration to use panolint
    - fixes RuboCop warnings

--- spec/sqlite3_spec.rb
+++ spec/sqlite3_spec.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 require "shared/unique_attributes_examples"
 require "config/test_setup_migration"
 
 RSpec.describe "SQLite3" do
-  before :all do
+  before :all do # rubocop:disable RSpec/BeforeAfterAll
     ActiveRecord::Base.establish_connection(:sqlite3_test)
     TestSetupMigration.migrate(:up)
   end
 
-  include_examples ".unique_attribute"
-
-  after :all do
+  after :all do # rubocop:disable RSpec/BeforeAfterAll
     ActiveRecord::Base.remove_connection
   end
+
+  include_examples ".unique_attribute"
 end
